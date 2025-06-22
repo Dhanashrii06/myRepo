24dc983ac2fc352bbf72ef818aeebe68e34f4fab
dhanashri <dchitukane@gmail.com>
2025-06-22 14:05:23 +0530

    v4

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const pi=2.3;
 var a;
 var b=3;
 
-console.log(a);
+console.log(b);
+console.log(myName);
+console.log(myName.length-1);
+var arr=["jon",2123];
+console.log(arr.pop());
+function name(){
+    console.log("heiiik");
+}
+name()
+name()
+name()
